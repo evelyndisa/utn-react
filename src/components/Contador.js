cb0e895f3ddc72cbc3e13d93b1478c2083dc7c92
evelyndisa <evelyndisa@icloud.com>
2023-06-15 21:32:32 -0300

    contador de productos

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
+
 export default function Contador (){
+/*
+    informacion general: 
+        -cantidad
+        -precio unitario
+        -total
+
+    info fija:
+        -precio unitario
+
+    predeterminados:
+        -cantidad: 0
+        -precio unitario:500
+        -total: 0
+
+*/
+    const precioUnitario = 500;
+
+    const sumar = ()=>{
+        setCantidad(cantidad + 1)
+    }
+    const restar = ()=>{
+        if(cantidad > 0){
+            setCantidad(cantidad - 1)
+        }
+    }
+
+    const [cantidad, setCantidad] = useState(1)
+
     return(
         <div>
-            <p>Precio unitario: $#####</p>
+            <p>Precio unitario: ${precioUnitario}</p>
             <div>
-                <button>Restar -</button>
-                <p>CANTIDAD</p>
-                <button>Sumar +</button>
+                <button onClick={restar}>Restar -</button>
+                <p>{cantidad}</p>
+                <button onClick={sumar}>Sumar +</button>
             </div>
-            <p>Total: $NNNNN</p>
+            <p>Total: ${cantidad * precioUnitario}</p>
         </div>
     )
 }
